343e3de1ced98160bd6a9c795a9ccd9fcc6939e2
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-18 01:54:14 +0200

    slideshow Komponente erstellt

---
+++ src/components/slideshow/slideshow.tsx
@@ -0,0 +1,13 @@
+import { Component,  h } from '@stencil/core';
+
+@Component({
+  tag: 'slideshow-component',
+  styleUrl: 'slideshow.css',
+  shadow: true,
+})
+export class Slideshow {
+
+  render() {
+    return <div></div>;
+  }
+}
