5fd4a5e980fabcf59c71787aa617fb30571d7c7a
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-18 01:39:28 +0200

    hab das Modul für die Links erstellt

---
+++ src/components/links/links.tsx
@@ -0,0 +1,13 @@
+import { Component,  h } from '@stencil/core';
+
+@Component({
+  tag: 'links-head-component',
+  styleUrl: 'links.css',
+  shadow: true,
+})
+export class LinksHeader {
+
+  render() {
+    return <div></div>;
+  }
+}
