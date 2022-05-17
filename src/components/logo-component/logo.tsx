d8ba48760b72c07a45e47f745ef372e06d5911ab
JendrikGit <jgweiss@gmx.de>
2022-05-18 01:33:57 +0200

    render Logo + cssdatei

---
+++ src/components/logo-component/logo.tsx
@@ -0,0 +1,16 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+
+  tag: 'logo-component',
+  styleUrl: 'logo.css',
+  shadow: true,
+
+})
+export class Logo {
+//rendern des Logos
+  render() {
+
+    return <div></div>;
+  }
+}
