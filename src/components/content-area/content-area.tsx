cf0b21b6f2b928f6f2908e36330792ab423e08fa
SebLudwig <Sebastian.D.Ludwig@icloud.com>
2022-05-18 01:08:42 +0200

    Cre8 content-area

---
+++ src/components/content-area/content-area.tsx
@@ -0,0 +1,13 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'content-area-component',
+  styleUrl: 'content-area.css',
+  shadow: true,
+})
+export class ContentArea {
+
+  render() {
+    return <div></div>;
+  }
+}
