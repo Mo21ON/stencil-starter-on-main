94a19f896b3d97f1dc5a1c54603bfbb2f9042b50
JendrikGit <jgweiss@gmx.de>
2022-05-20 23:52:02 +0200

    logo pic and css

--- src/components/logo-component/logo.tsx
+++ src/components/logo-component/logo.tsx
@@ -1,16 +1,16 @@
-import { Component, h } from '@stencil/core';
+import { Component, getAssetPath, h, Prop } from '@stencil/core';
 
 @Component({
-
   tag: 'logo-component',
   styleUrl: 'logo.css',
-  shadow: true,
-
+  shadow: false,
+  assetsDirs: ['assets']
 })
 export class Logo {
-//rendern des Logos
-  render() {
 
-    return <div></div>;
+  @Prop() imagename: string;
+
+  render() {
+    return <div id="logo"><img src={getAssetPath(`./assets/${this.imagename}`)} alt={this.imagename}/></div>;
   }
 }
