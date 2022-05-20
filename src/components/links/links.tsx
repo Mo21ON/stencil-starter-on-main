8d9a624fd6705e1bcca969908f4ca415a555c2d9
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-21 00:46:52 +0200

    update Links-component, update global css

--- src/components/links/links.tsx
+++ src/components/links/links.tsx
@@ -3,11 +3,32 @@ import { Component,  h } from '@stencil/core';
 @Component({
   tag: 'links-head-component',
   styleUrl: 'links.css',
-  shadow: true,
+  shadow: false,
 })
 export class LinksHeader {
+  private links: LinkData[] = [{
+    label: "Über uns",
+    url: "http://test.de/"
+  },{
+    label: "Impressum",
+    url: "http://test1.de/"
+  },{
+    label: "Produkte",
+    url: "http://test2.de/"
+  },{
+    label: "Mitglieder",
+    url: "http://test2.de/"
+  }];
 
   render() {
-    return <div></div>;
+    return <div id="links">
+      {this.links.map((l) =>(<a href={l.url}>{l.label}</a>))}
+    </div>;
   }
 }
+
+interface LinkData{
+  label: string;
+  url: string;
+}
+
