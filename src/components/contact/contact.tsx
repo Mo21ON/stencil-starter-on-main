c3b49c1ca10be1631c35271d90fd9806ac24d05c
giterator21 <fel.kopp.21@lehre.mosbach.dhbw.de>
2022-05-27 18:02:11 +0200

    some changes

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -10,6 +10,14 @@ export class Contact {
   @Prop() mail: string;
 
   render() {
-    return <Host id="contact"><a href={'mailto:'+this.mail}>{this.mail}</a></Host>;
+    return <Host id="contact"><a href={'mailto:'+this.mail}>{this.mail}</a>;
+
+    <p>Tel.: 06261 9390</p>
+    <p> </p>
+    <p>MOS-Fit Consulting Gmbh</p>
+    <p> </p>
+    <p>Winkelgasse 5A</p>
+    <p> </p>
+    <p>74821 Mosbach</p></Host>;
   }
 }
